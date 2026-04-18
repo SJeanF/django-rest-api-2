c6a6786d8b49b355601a2b7346d4039159b703d5
SJeanF <fonsecasantosjean@gmail.com>
2026-04-18 13:39:00 -0300

    fix: concertar erro ao requisitar as order

--- bookstore/urls.py
+++ bookstore/urls.py
@@ -21,6 +21,6 @@ from django.urls import path, include, re_path
 urlpatterns = [
     path('__debug__/', include(debug_toolbar.urls)),    
     path("admin/", admin.site.urls),
-    re_path("bookstore/(?P<version>(v1|v2))", include("order.urls")),
+    re_path("bookstore/(?P<version>(v1|v2))/", include("order.urls")),
     re_path("bookstore/(?P<version>(v1|v2))/", include("product.urls")),
 ]
