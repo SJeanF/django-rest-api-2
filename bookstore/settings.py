69f4e73a4315e5073cea46f6978371d45e3a569c
SJeanF <fonsecasantosjean@gmail.com>
2026-07-29 16:38:54 -0300

    fix: adicionar allowed_host

--- bookstore/settings.py
+++ bookstore/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-r#d$h-f%fn#o1y+bn2*y9-q+f$4_e-2j3xcx(u)q0jbjfi&79z
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
